9d749ffee3e37f38ff21fc3557280bb71a74d856
rodkrtz <rodkrtz@outlook.com>
2021-02-14 11:00:28 -0300

    Tratamento de exceção ao adicionar equipamento.

--- controle-manutencao/src/main/java/br/com/rodkrtz/projuris/controle/manutencao/service/impl/EquipamentoServiceImpl.java
+++ controle-manutencao/src/main/java/br/com/rodkrtz/projuris/controle/manutencao/service/impl/EquipamentoServiceImpl.java
@@ -2,14 +2,13 @@ package br.com.rodkrtz.projuris.controle.manutencao.service.impl;
 
 import br.com.rodkrtz.projuris.controle.manutencao.dao.ClienteDao;
 import br.com.rodkrtz.projuris.controle.manutencao.dao.EquipamentoDao;
+import br.com.rodkrtz.projuris.controle.manutencao.exception.ExceptionType;
 import br.com.rodkrtz.projuris.controle.manutencao.model.entity.Cliente;
 import br.com.rodkrtz.projuris.controle.manutencao.model.entity.Equipamento;
 import br.com.rodkrtz.projuris.controle.manutencao.model.request.AddEquipamentoRequest;
 import br.com.rodkrtz.projuris.controle.manutencao.service.EquipamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.NoSuchElementException;
 
 /**
  * @author Rodrigo Kreutzfeld
@@ -33,12 +32,12 @@ public class EquipamentoServiceImpl implements EquipamentoService {
 
         Cliente cliente = clienteDao.findByEmail(addEquipamentoRequest.getEmailCliente());
         if (cliente == null) {
-            throw new NoSuchElementException("Não foi encontrado um cliente com esse email ".concat(addEquipamentoRequest.getEmailCliente()));
+            ExceptionType.BAD_REQUEST.throwException("Não foi encontrado um cliente com esse email ".concat(addEquipamentoRequest.getEmailCliente()));
         }
 
         Equipamento equipamento = equipamentoDao.findByNumeroSerie(addEquipamentoRequest.getNumeroSerieEquipamento());
         if (equipamento != null) {
-            throw new IllegalArgumentException("Já existe um equipamento registrado com esse número de série");
+            ExceptionType.BAD_REQUEST.throwException("Já existe um equipamento registrado com esse numero de série ".concat(equipamento.getNumeroSerie()));
         }
 
         equipamento = new Equipamento()
